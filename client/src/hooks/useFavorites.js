025a17f8c19b2d81b5a5b4c836d52babf085b5be
Martina García Rios <martinamgarciarios@gmail.com>
2026-06-22 22:14:53 -0300

    Corregimos la persistencia de los productos favoritos al iniciar sesion

--- client/src/hooks/useFavorites.js
+++ client/src/hooks/useFavorites.js
@@ -1,18 +1,17 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setFavorites } from '../store/slices/favoriteSlice';
 import { favoritosApi } from '../api/favoritosApi';
 
 export function useFavorites() {
   const dispatch = useDispatch();
+  const userId = useSelector(state => state.user?.user?.id);
 
   useEffect(() => {
-    const localUser = JSON.parse(localStorage.getItem("user") || "{}");
-    const userId = localUser?.id;
     if (!userId) return;
 
     favoritosApi.getAll(userId)
       .then(data => dispatch(setFavorites(data)))
       .catch(err => console.error("Error al cargar favoritos:", err));
-  }, [dispatch]);
+  }, [dispatch, userId]);
 }
